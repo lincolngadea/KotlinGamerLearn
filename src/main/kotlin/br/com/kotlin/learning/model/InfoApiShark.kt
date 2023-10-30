d4cfb981524c949a384b56e99c8ba30211e486d7
Lincoln Gadea <lincoln@glinc.com.br>
2023-10-30 19:24:19 -0300

    chore: create new structure to game

---
+++ src/main/kotlin/br/com/kotlin/learning/model/InfoApiShark.kt
@@ -0,0 +1,3 @@
+package br.com.kotlin.learning.model
+
+data class InfoApiShark(val title:String, val thumb:String)
---
+++ src/main/kotlin/br/com/kotlin/learning/principal/Main.kt
@@ -0,0 +1,49 @@
+package br.com.kotlin.learning.principal
+import br.com.kotlin.learning.model.Jogo
+import br.com.kotlin.learning.service.ConsumoApi
+import java.util.Scanner
+
+
+fun main() {
+    val leitura = Scanner(System.`in`)
+    println("Digite um código de jogo para buscar:")
+    val busca = leitura.nextLine()
+
+    val buscaApi = ConsumoApi()
+    val informacaoJogo = buscaApi.buscaJogo(busca)
+
+
+    var meuJogo: Jogo? = null
+
+    val resultado = runCatching {
+        meuJogo = Jogo(
+            informacaoJogo.info.title,
+            informacaoJogo.info.thumb
+        )
+    }
+
+    resultado.onFailure {
+        println("Jogo inexistente. Tente outro id.")
+    }
+
+    resultado.onSuccess {
+        println("Deseja inserir uma descrição personalizada? S/N")
+        val opcao = leitura.nextLine()
+        if (opcao.equals("s", true)) {
+            println("Insira a descrição personalizado para o jogo:")
+            val descricaoPersonalizada = leitura.nextLine()
+            meuJogo?.descricao = descricaoPersonalizada
+        } else {
+            meuJogo?.descricao = meuJogo?.titulo
+
+        }
+
+        println(meuJogo)
+    }
+
+    resultado.onSuccess {
+        println("Busca finalizada com sucesso.")
+    }
+
+
+}
