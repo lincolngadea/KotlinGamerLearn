615e11dbb3a9367408fa9cf10b60aa606a6c8cf0
Lincoln Gadea <lincoln@glinc.com.br>
2023-11-03 10:57:53 -0300

    refact: add loop to check if the user wants new searchs

--- src/main/kotlin/br/com/kotlin/learning/principal/Main.kt
+++ src/main/kotlin/br/com/kotlin/learning/principal/Main.kt
@@ -6,39 +6,41 @@ import java.util.Scanner
 
 fun main() {
     val read = Scanner(System.`in`)
-    println("Type a game code to search:")
-    val searchId = read.nextLine()
-
     var myGame: Game? = null
-
     val apiSearch = ApiConsummer()
+    do {
+        println("Type a game code to search:")
+        val searchId = read.nextLine()
 
-    apiSearch.gameSearch(searchId).onSuccess {
-        val resultGameObject = runCatching {
-            myGame = Game(
-                it.info.title,
-                it.info.thumb
-            )
-        }
-        resultGameObject.onFailure {
-            println("Game not found. try again other id.")
-        }
-
-        resultGameObject.onSuccess {
-            println("Do you want insert a custom description? Y/N")
-            val option = read.nextLine()
-            if (option.equals("y", true)) {
-                println("Insert a custom description in the game:")
-                val customDescription = read.nextLine()
-                myGame?.description = customDescription
-            } else {
-                myGame?.description = myGame?.title
+        apiSearch.gameSearch(searchId).onSuccess {
+            val resultGameObject = runCatching {
+                myGame = Game(
+                    it.info.title,
+                    it.info.thumb
+                )
             }
-            println(myGame)
-        }
+            resultGameObject.onFailure {
+                println("Game not found. try again other id.")
+            }
 
-        resultGameObject.onSuccess {
-            println("Success end search.")
+            resultGameObject.onSuccess {
+                println("Do you want insert a custom description? Y/N")
+                val option = read.nextLine()
+                if (option.equals("y", true)) {
+                    println("Insert a custom description in the game:")
+                    val customDescription = read.nextLine()
+                    myGame?.description = customDescription
+                } else {
+                    myGame?.description = myGame?.title
+                }
+                println(myGame)
+            }
+
+            resultGameObject.onSuccess {
+                println("Success end search.")
+            }
         }
-    }
+        println("Do you want a new search?Y/N")
+        val ask = read.nextLine()
+    } while (ask.equals("Y", true))
 }
