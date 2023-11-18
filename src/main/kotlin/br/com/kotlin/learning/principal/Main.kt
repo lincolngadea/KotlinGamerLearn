6c30e8abab3b1db146630bc96691029867c3499d
Lincoln Gadea <lincoln@glinc.com.br>
2023-11-18 14:52:24 -0300

    chore: create adGames function

--- src/main/kotlin/br/com/kotlin/learning/principal/Main.kt
+++ src/main/kotlin/br/com/kotlin/learning/principal/Main.kt
@@ -1,18 +1,24 @@
 package br.com.kotlin.learning.principal
 
 import br.com.kotlin.learning.model.Game
+import br.com.kotlin.learning.repository.DataBase
 import br.com.kotlin.learning.repository.GamesDAO
 
 fun main() {
     val game = Game(
-        "The Last of Us Part I",
+        "The Last of Us Part II",
         "https://cdn.cloudflare.steamstatic.com/steam/apps/1888930/header.jpg?t=1686864554",
         5.99,
         "Uma aventura pós-apocalíptica de sobrevivência em um mundo infestado por zumbis e facções em conflito."
     )
-    val gamesDAO = GamesDAO()
+
+    val manager = DataBase.getEntityManager()
+    val gamesDAO = GamesDAO(manager)
 //    jogoDAO.adicionarJogo(jogo)
 
+    gamesDAO.addGames(game)
     val gamesList: List<Game> = gamesDAO.getGames()
     println(gamesList)
+
+    manager.close()
 }
--- src/main/kotlin/br/com/kotlin/learning/repository/GamesDAO.kt
+++ src/main/kotlin/br/com/kotlin/learning/repository/GamesDAO.kt
@@ -2,25 +2,33 @@ package br.com.kotlin.learning.repository
 
 import br.com.kotlin.learning.entities.GameEntity
 import br.com.kotlin.learning.model.Game
+import javax.persistence.EntityManager
 
-class GamesDAO {
+class GamesDAO(private val manager: EntityManager) {
 
-    fun getGames(): List<Game>{
-        val manager = DataBase.getEntityManager()
-        try {
-            val query = manager.createQuery("FROM GameEntity", GameEntity::class.java)
-            val gameEntity = query.resultList
-            val game = gameEntity.map {
-                Game(
-                    it.titulo,
-                    it.capa,
-                    it.preco,
-                    it.descricao
-                )
-            }
-            return game
-        }finally {
-            manager.close()
+    fun getGames(): List<Game> {
+        val query = manager.createQuery("FROM GameEntity", GameEntity::class.java)
+        val gameEntity = query.resultList
+        val game = gameEntity.map {
+            Game(
+                it.titulo,
+                it.capa,
+                it.preco,
+                it.descricao
+            )
         }
+        return game
+    }
+
+    fun addGames(game: Game) {
+        val entity = GameEntity(
+            capa = game.thumb,
+            descricao = game.description,
+            preco = game.price,
+            titulo = game.title
+        )
+        manager.transaction.begin()
+        manager.persist(entity)
+        manager.transaction.commit()
     }
 }
